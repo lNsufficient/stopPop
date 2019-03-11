fda2066a321c3ff782603c7f84d0eeffa5b210e8
lNsufficient <edvard.jo@gmail.com>
2019-03-11 21:46:26 +0100

    Slightly changed limit so that micro was removed

--- pop_tools.py
+++ pop_tools.py
@@ -9,7 +9,7 @@ def threshold_pop(a):
     median_volume = np.percentile(abs_a, 85)
     max_volume = np.percentile(abs_a, 99)
     #remove all sound that's background:
-    background_threshold = median_volume + (max_volume-median_volume)*0.1
+    background_threshold = median_volume + (max_volume-median_volume)*0.2
     thresh_a = a.copy()
     thresh_a[np.abs(thresh_a)<background_threshold] = 0
     return thresh_a, background_threshold
